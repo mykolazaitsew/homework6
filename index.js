c2e1822ffb38e4e938eebe881eb9a9c4e1d0f6f7
Zaitsev Mykola <zaitsev.nikolay11@gmail.com>
2024-05-28 18:47:52 +0300

    task3

--- index.js
+++ index.js
@@ -6,6 +6,26 @@ const task1 = (str) => {
 };
 
 
+const task2 = (str, length, symbol, toLeft) => {
+    
+}
 
+
+const task3 = (str, separator) => {
+    const splitStr = str.split(separator); 
+    const bigbukva = (i) => {
+      let camel = "";
+      for (let j = 1; j < splitStr.at(1).length; j++) {
+        camel = `${camel}${splitStr.at(i).at(j)}`;
+      }
+      return `${splitStr.at(i).at(0).toUpperCase()}${camel}`;
+    };
+    let camel = "";
+    for (let i = 1; i < splitStr.length; i++) {
+        camel = `${camel}${bigbukva(i)}`;
+    }
+   return`${splitStr.at(0)}${camel}`
+  };
 
 console.log(task1('hello'));
+console.log(task3('hello world kolya', ' '));
